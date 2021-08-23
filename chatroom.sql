30b69b352ccc70d3ce82476845e633d49acd87e7
Ewell <dabau975@gmail.com>
2021-08-23 09:52:22 +0800

    sql

--- chatroom.sql
+++ chatroom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2021-08-19 11:12:36
+-- 產生時間： 2021-08-23 03:50:37
 -- 伺服器版本： 10.4.20-MariaDB
 -- PHP 版本： 8.0.9
 
@@ -51,7 +51,6 @@ INSERT INTO `chat_msg` (`username`, `msg`, `timestamp`, `purpose`) VALUES
 ('Steven', '話不要這麼多啦', '15:57', 'group'),
 ('Steven', 'YOOOOOOOOOOOO', '15:58', 'group'),
 ('May', 'TESTTEST', '16:15', 'group'),
-('COCO', '安安你好', '16:46', 'undefined'),
 ('COCO', '安安你好', '16:47', 'May'),
 ('COCO', 'YOYOYO', '16:48', 'Jan'),
 ('Jan', '吃飯去 gogo', '16:52', 'Steven'),
@@ -74,7 +73,24 @@ INSERT INTO `chat_msg` (`username`, `msg`, `timestamp`, `purpose`) VALUES
 ('Steven', '又舔 又舔嘴唇', '15:31', 'group'),
 ('Steven', '123123123', '15:42', 'Jan'),
 ('Jan', '666666666666', '15:54', 'May'),
-('Jan', '5555555555', '15:55', 'May');
+('Jan', '5555555555', '15:55', 'May'),
+('COCO', 'qwtwqagwgqw', '08:57', 'group'),
+('May', 'babqb', '08:57', 'group'),
+('COCO', '1231321313231', '08:58', 'May'),
+('May', 'agqawg1w', '08:58', 'COCO'),
+('COCO', '不要笑 不要笑', '09:05', 'group'),
+('COCO', 'LOLLLLLLLLLLLL', '09:06', 'May'),
+('May', '12312312312312', '09:06', 'COCO'),
+('COCO', '123123', '10:10', 'group'),
+('COCO', '246246', '10:11', 'group'),
+('COCO', '66666666666666', '10:11', 'May'),
+('COCO', '777777777777777', '10:11', 'May'),
+('COCO', 'safadndf', '10:42', 'group'),
+('COCO', 'ananwqnwq', '10:42', 'group'),
+('COCO', 'sabwqqwh', '10:42', 'group'),
+('May', 'qhqwwqh', '10:42', 'group'),
+('May', '213123123123123132312', '10:42', 'COCO'),
+('May', '5555555555', '09:34', 'Jan');
 
 -- --------------------------------------------------------
 
@@ -85,7 +101,7 @@ INSERT INTO `chat_msg` (`username`, `msg`, `timestamp`, `purpose`) VALUES
 CREATE TABLE `user` (
   `id` int(11) NOT NULL COMMENT '自增ID',
   `username` varchar(255) NOT NULL,
-  `password` varchar(32) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `token` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -94,10 +110,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `token`) VALUES
-(15, 'COCO', '12345', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNvY28iLCJwYXNzd29yZCI6IjEyMzQ1IiwiaWF0IjoxNjI5MTYwNjMwLCJleHAiOjE2MjkxNjQyMzB9.ubT6-xmWY27CynKPFyH64IWrZ8ONS8x6UwnvIpsyAPI'),
-(16, 'Steven', '12345', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0ZXZlbiIsInBhc3N3b3JkIjoiMTIzNDUiLCJpYXQiOjE2MjkxNzczNTQsImV4cCI6MTYyOTE4MDk1NH0.RgE44mvNPOCVdQC79SNmorEYNoc27pVzN4zzhbRkxa4'),
-(17, 'May', '12345', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik1heSIsInBhc3N3b3JkIjoiMTIzNDUiLCJpYXQiOjE2MjkyNDk3NTgsImV4cCI6MTYyOTI1MzM1OH0.Z5s1lL72_7fNteA-O3_iCDgUWseGntp72GmfNU5suFQ'),
-(19, 'Jan', '12345', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkphbiIsInBhc3N3b3JkIjoiMTIzNDUiLCJpYXQiOjE2MjkyNTMxNTksImV4cCI6MTYyOTI1Njc1OX0.qBvneckUGZbS-tTb0-g3qgW_QFbgidPd4w_3x1xDrGw');
+(15, 'COCO', '$2b$10$5ICL6smzD5TzxegERO.LIOVxKS2y6e6qVNpnWozkH1qXtyrcbU7vu', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNvY28iLCJwYXNzd29yZCI6IjEyMzQ1IiwiaWF0IjoxNjI5MTYwNjMwLCJleHAiOjE2MjkxNjQyMzB9.ubT6-xmWY27CynKPFyH64IWrZ8ONS8x6UwnvIpsyAPI'),
+(16, 'Steven', '$2b$10$mLp2dY1Hm8NoFuOW9No8QupMCkd6yp3sRmnzZJHSitl/PHB6y5BbG', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0ZXZlbiIsInBhc3N3b3JkIjoiMTIzNDUiLCJpYXQiOjE2MjkxNzczNTQsImV4cCI6MTYyOTE4MDk1NH0.RgE44mvNPOCVdQC79SNmorEYNoc27pVzN4zzhbRkxa4'),
+(17, 'May', '$2b$10$5AxcZwfnS7ViqfW6qGHP5OoDofPNzV9K0bz84/48/Hx696pWKj4HS', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ik1heSIsInBhc3N3b3JkIjoiMTIzNDUiLCJpYXQiOjE2MjkyNDk3NTgsImV4cCI6MTYyOTI1MzM1OH0.Z5s1lL72_7fNteA-O3_iCDgUWseGntp72GmfNU5suFQ'),
+(19, 'Jan', '$2b$10$1Pc8a./uHzzLlLDhNO5.puxq.ov60N/yQubZRFSVLZT5lQSnYizuW', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkphbiIsInBhc3N3b3JkIjoiMTIzNDUiLCJpYXQiOjE2MjkyNTMxNTksImV4cCI6MTYyOTI1Njc1OX0.qBvneckUGZbS-tTb0-g3qgW_QFbgidPd4w_3x1xDrGw');
 
 --
 -- 已傾印資料表的索引
@@ -118,7 +134,7 @@ ALTER TABLE `user`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID', AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID', AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
